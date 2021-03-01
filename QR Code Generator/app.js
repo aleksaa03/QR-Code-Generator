f2b2ff34aa8d2c6c029ab7cf37b1f541ca1eda05
Aleksa <71262651+aleksaa03@users.noreply.github.com>
2021-03-01 22:54:56 +0100

    Add files via upload

--- QR Code Generator/app.js
+++ QR Code Generator/app.js
@@ -1,3 +1,6 @@
+var colorPicker = document.getElementById("color");
+var block = document.getElementById("block");
+
 function generateQR() {
   var url = document.getElementById("url").value;
   var height = document.getElementById("height").value;
@@ -7,13 +10,34 @@ function generateQR() {
 
   qrCodeDiv.innerHTML = "";
 
+  var color = colorPicker.value;
+  var r = parseInt(color.substr(1, 2), 16);
+  var g = parseInt(color.substr(3, 2), 16);
+  var b = parseInt(color.substr(5, 2), 16);
+
+  if (r < 240 || g < 235 || b < 215) {
+    color = colorPicker.value;
+  } else {
+    color = "#000000";
+  }
+
   if (!isNaN(height) && !isNaN(width)) {
     if (height <= 200 && width <= 200) {
       var qrcode = new QRCode(qrCodeDiv, {
         text: url,
         width: height,
         height: width,
+        colorDark: color,
+        colorLight: "#ffffff",
       });
     }
   }
+}
+
+function openColorPicker() {
+  colorPicker.click();
+}
+
+function getColor(e) {
+  block.style.background = e.target.value;
 }
